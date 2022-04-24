b096ec6aff7d9a20a786cb79ed0b2086376f6330
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-04-24 11:52:44 +0200

    [JRD-5] change component name in Profile.tsx

--- src/components/pages/Profile/Profile.tsx
+++ src/components/pages/Profile/Profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const SignIn: React.FC<Record<string, unknown>> = () => {
+const Profile: React.FC<Record<string, unknown>> = () => {
   return <div>Profile</div>;
 };
 
-export default SignIn;
+export default Profile;
