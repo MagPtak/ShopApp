7c55bc19570e9a9590a5b63b646eec82777441b5
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-05-23 11:32:54 +0200

    [JRD-27] add photos to category component

--- src/components/pages/Category/Category.tsx
+++ src/components/pages/Category/Category.tsx
@@ -4,9 +4,23 @@ import "./Category.css";
 
 const Category: React.FC<Record<string, unknown>> = () => {
   let { name } = useParams();
+  let image;
+
+  if (name === "clothing") {
+    image = "../../../assets/category/clothing.jpg";
+  } else if (name === "shoes") {
+    image = "../../../assets/category/shoes.jpg";
+  } else if (name === "accessories") {
+    image = "../../../assets/category/accessories.jpg";
+  } else {
+    image = "../../../assets/category/gadgets.jpg";
+  }
+
   return (
     <article className="container">
-      <div className="categoryContent">Category {name ? name : null}</div>
+      <div className="categoryContent">
+        <p>Category {name ? name : null}</p>
+      </div>
     </article>
   );
 };
