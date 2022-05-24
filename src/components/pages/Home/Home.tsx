430399e8836926db2deee2440653102c0ce6a981
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-05-24 09:58:35 +0200

    [JRD-7] add eslint fixes

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import Main from './Main/Main'
-import CategorySection from '../../CategorySection/CategorySection';
+import React from "react";
+import Main from "./Main/Main";
+import CategorySection from "../../CategorySection/CategorySection";
 
-const Home: React.FC<Record<string, unknown>> = () => { 
- return (
-    <div className='container'>
+const Home: React.FC<Record<string, unknown>> = () => {
+  return (
+    <div className="container">
       <Main />
       <CategorySection />
     </div>
-  )
-}
+  );
+};
 
 export default Home;
-
